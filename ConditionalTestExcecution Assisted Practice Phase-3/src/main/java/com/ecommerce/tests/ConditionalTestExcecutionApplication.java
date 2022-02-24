9ca32d3bc99ca24f687e9d7afbd522f940d54943
MohammedOwais52 <63663793+MohammedOwais52@users.noreply.github.com>
2022-02-24 13:59:36 +0530

    Add files via upload

---
+++ ConditionalTestExcecution Assisted Practice Phase-3/src/main/java/com/ecommerce/tests/ConditionalTestExcecutionApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalTestExcecutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionalTestExcecutionApplication.class, args);
+	}
+
+}
