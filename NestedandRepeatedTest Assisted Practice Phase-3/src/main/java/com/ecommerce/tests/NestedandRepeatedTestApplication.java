47310e76359bdcc18e73c96ad431f088429fd8f6
MohammedOwais52 <63663793+MohammedOwais52@users.noreply.github.com>
2022-02-24 14:07:32 +0530

    Add files via upload

---
+++ NestedandRepeatedTest Assisted Practice Phase-3/src/main/java/com/ecommerce/tests/NestedandRepeatedTestApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestedandRepeatedTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestedandRepeatedTestApplication.class, args);
+	}
+
+}
