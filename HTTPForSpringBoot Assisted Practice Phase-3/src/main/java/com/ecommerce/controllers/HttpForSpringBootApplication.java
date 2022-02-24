47c47f993e823be3d4a0c9e19c5e8cb3bc3058b7
MohammedOwais52 <63663793+MohammedOwais52@users.noreply.github.com>
2022-02-24 14:04:47 +0530

    Add files via upload

---
+++ HTTPForSpringBoot Assisted Practice Phase-3/src/main/java/com/ecommerce/controllers/HttpForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpForSpringBootApplication.class, args);
+	}
+
+}
---
+++ JUnit5StandardTest Assisted Practice Phase-3/src/main/java/com/ecommerce/tests/JUnit5StandardTestApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnit5StandardTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnit5StandardTestApplication.class, args);
+	}
+
+}
